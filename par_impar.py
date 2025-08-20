1bb68800a4dee8e73e6798ecc21d4669a298eef1
VITOR MANOEL GOMES DA SILVA <vitormanoelsilva2007@gmail.com>
2025-08-20 11:20:53 -0300

    Init: Iniciando o estudo em Python

---
+++ par_impar.py
@@ -0,0 +1,6 @@
+# declarar a variável que ir guardar um número
+valor = 13
+if(valor % 2 == 0):
+    print("Este valor é Par")
+else:
+    print("Eate valor é Impar")
---
+++ pontoevirgula.py
@@ -0,0 +1,3 @@
+# Declaração de variávelcomconteúdo e separação utilizando ponto e virgula
+pais = "Brasil" ; uf = "SP" ; cidade = "Suzano" ; espaco = " - "
+print(pais + espaco + uf + espaco + cidade)

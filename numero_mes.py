d2ec7f91433680e47199f51fb4f287eba30b5499
VITOR MANOEL GOMES DA SILVA <vitormanoelsilva2007@gmail.com>
2025-08-25 08:18:44 -0300

    Mês

---
+++ numero_mes.py
@@ -0,0 +1,17 @@
+# Este programa pede um número que representa  mês por extenso
+num = input("Digite o número do mês:  ")
+
+match num:
+    case '1': print("Janeiro")
+    case '2': print("Fevereiro")
+    case '3': print("Março")
+    case '4': print("Abril")
+    case '5': print("Maio")
+    case '6': print("Junho")
+    case '7': print("Julho")
+    case '8': print("Agosto")
+    case '9': print("Setembro")
+    case '10': print("Outubro")
+    case '11': print("Novembro")
+    case '12': print("Dezembro")
+    case _: print("Mês inválido")
